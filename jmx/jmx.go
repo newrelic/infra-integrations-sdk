d36a89abd1bfea5b7698f770f47cdc163802bbde
Toni Reina <treina@newrelic.com>
2017-07-24 19:34:56 +0200

    Add documentation about jmx go library

--- jmx/jmx.go
+++ jmx/jmx.go
@@ -1,3 +1,7 @@
+/*
+Package jmx is a library to get metrics through JMX. It requires additional
+setup. Read https://github.com/newrelic/infra-integrations-sdk#JMX-SUPPORT for
+instructions. */
 package jmx
 
 import (
