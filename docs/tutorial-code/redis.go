243c78f7ec4c6bac0100e368505c0f0a631b4733
Ruben Hervas <rhervas@newrelic.com>
2018-07-04 15:47:37 +0200

    update tutorial code

--- docs/tutorial-code/redis.go
+++ docs/tutorial-code/redis.go
@@ -25,119 +25,88 @@ const (
 
 var args argumentList
 
-func populateInventory(e *integration.Entity) error {
-	cmd := exec.Command("/bin/sh", "-c", "redis-cli CONFIG GET dbfilename")
+func queryRedisInfo(query string) (float64, error) {
+	cmd := exec.Command("/bin/sh", "-c", fmt.Sprintf("redis-cli info | grep %s", query))
 	output, err := cmd.CombinedOutput()
 	if err != nil {
-		return err
-	}
-
-	splittedLine := strings.Split(string(output), "\n")
-	if splittedLine[0] == "dbfilename" {
-		err = e.SetInventoryItem(splittedLine[0], "value", splittedLine[1])
-		if err != nil {
-			return err
-		}
-	}
-
-	cmd = exec.Command("/bin/sh", "-c", "redis-cli CONFIG GET bind")
-	output, err = cmd.CombinedOutput()
-	if err != nil {
-		return err
-	}
-	splittedLine = strings.Split(string(output), "\n")
-	if splittedLine[0] == "bind" {
-		err = e.SetInventoryItem(splittedLine[0], "value", splittedLine[1])
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func populateMetrics(ms *metric.Set) error {
-	cmd := exec.Command("/bin/sh", "-c", "redis-cli info | grep instantaneous_ops_per_sec:")
-	output, err := cmd.CombinedOutput()
-	if err != nil {
-		return err
+		return 0, err
 	}
 	splittedLine := strings.Split(string(output), ":")
 	if len(splittedLine) != 2 {
-		return fmt.Errorf("cannot split the output line")
+		return 0, fmt.Errorf("Cannot split the output line")
 	}
-	metricValue, err := strconv.ParseFloat(strings.TrimSpace(splittedLine[1]), 64)
-	if err != nil {
-		return err
-	}
-	ms.SetMetric("query.instantaneousOpsPerSecond", metricValue, metric.GAUGE) // nolint: errcheck
-
-	cmd = exec.Command("/bin/sh", "-c", "redis-cli info | grep total_connections_received:")
-	output, err = cmd.CombinedOutput()
-	if err != nil {
-		return err
-	}
-	splittedLine = strings.Split(string(output), ":")
-	if len(splittedLine) != 2 {
-		return fmt.Errorf("cannot split the output line")
-	}
-	metricValue, err = strconv.ParseFloat(strings.TrimSpace(splittedLine[1]), 64)
-	if err != nil {
-		return err
-	}
-
-	ms.SetMetric("net.connectionsReceivedPerSecond", metricValue, metric.RATE) // nolint: errcheck
-	return nil
-}
-
-func populateEvents(e *integration.Entity) error {
-	cmd := exec.Command("/bin/sh", "-c", "redis-cli info | grep uptime_in_seconds:")
-	output, err := cmd.CombinedOutput()
-	if err != nil {
-		return err
-	}
-	splittedLine := strings.Split(string(output), ":")
-	if len(splittedLine) != 2 {
-		return fmt.Errorf("cannot split the output line")
-	}
-	uptime, err := strconv.ParseFloat(strings.TrimSpace(splittedLine[1]), 64)
-	if err != nil {
-		return err
-	}
-	if uptime < 60 {
-		err = e.AddEvent(event.NewNotification("Redis Server recently started"))
-		if err != nil {
-			return err
-		}
-	}
-	if uptime < 60 {
-		err = e.AddEvent(event.New("Redis Server recently started", "redis-server"))
-	}
-
-	return err
+	return strconv.ParseFloat(strings.TrimSpace(splittedLine[1]), 64)
 }
 
 func main() {
+	// Create Integration
 	i, err := integration.New(integrationName, integrationVersion, integration.Args(&args))
 	panicOnErr(err)
 
-	e := i.LocalEntity()
+	// Create Entity, entities name must be unique
+	entity := i.LocalEntity()
+	panicOnErr(err)
 
-	if args.All() || args.Inventory {
-		panicOnErr(populateInventory(e))
-	}
-
-	if args.All() || args.Metrics {
-		ms, err := e.NewMetricSet("RedisSample")
-		panicOnErr(err)
-		panicOnErr(populateMetrics(ms))
-	}
-
+	// Add Event
 	if args.All() || args.Events {
-		err := populateEvents(e)
-		if err != nil {
-			i.Logger().Debugf("adding event failed, got: %s", err)
+		cmd := exec.Command("/bin/sh", "-c", "redis-cli info | grep uptime_in_seconds:")
+		output, err := cmd.CombinedOutput()
+		panicOnErr(err)
+
+		splittedLine := strings.Split(string(output), ":")
+		if len(splittedLine) != 2 {
+			panic(fmt.Errorf("Cannot split the output line"))
 		}
+		uptime, err := strconv.ParseFloat(strings.TrimSpace(splittedLine[1]), 64)
+		panicOnErr(err)
+		if uptime < 60 {
+			err = entity.AddEvent(event.NewNotification("Redis Server recently started"))
+		}
+		panicOnErr(err)
+		if uptime < 60 {
+			err = entity.AddEvent(event.New("Redis Server recently started", "redis-server"))
+		}
+
 	}
+
+	// Add Inventory item
+	if args.All() || args.Inventory {
+		cmd := exec.Command("/bin/sh", "-c", "redis-cli CONFIG GET dbfilename")
+		output, err := cmd.CombinedOutput()
+		panicOnErr(err)
+
+		splittedLine := strings.Split(string(output), "\n")
+		if splittedLine[0] == "dbfilename" {
+			err = entity.SetInventoryItem(splittedLine[0], "value", splittedLine[1])
+			panicOnErr(err)
+		}
+
+		cmd = exec.Command("/bin/sh", "-c", "redis-cli CONFIG GET bind")
+		output, err = cmd.CombinedOutput()
+		panicOnErr(err)
+
+		splittedLine = strings.Split(string(output), "\n")
+		if splittedLine[0] == "bind" {
+			err = entity.SetInventoryItem(splittedLine[0], "value", splittedLine[1])
+			panicOnErr(err)
+		}
+
+	}
+
+	// Add Metric
+	if args.All() || args.Metrics {
+		ms, err := entity.NewMetricSet("MyorgRedisSample")
+		panicOnErr(err)
+		metricValue, err := queryRedisInfo("instantaneous_ops_per_sec:")
+		panicOnErr(err)
+		err = ms.SetMetric("query.instantaneousOpsPerSecond", metricValue, metric.GAUGE)
+		panicOnErr(err)
+		metricValue1, err := queryRedisInfo("total_connections_received:")
+		panicOnErr(err)
+		err = ms.SetMetric("net.connectionsReceivedPerSecond", metricValue1, metric.RATE)
+		panicOnErr(err)
+	}
+
 	panicOnErr(i.Publish())
 }
 
