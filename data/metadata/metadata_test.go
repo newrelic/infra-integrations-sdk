d5c8ff1a8029fb872f464b8ef231250957e31f74
David Brota <dbrota@newrelic.com>
2020-04-17 17:20:50 +0200

    add metadata test

---
+++ data/metadata/metadata_test.go
@@ -0,0 +1,23 @@
+package metadata
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_Metadata_AddMetadataAndTags(t *testing.T) {
+	// given a new entity with no metadata
+	m := New("foobar", "test", "test nmetadata")
+
+	// when we add metadata and a tag
+	m.AddMetadata("m1", 55)
+	m.AddTag("t1", "hello there")
+
+	// then we check that the metadata hasn't changed
+	_, ok := m.Metadata["m1"]
+	assert.True(t, ok)
+
+	// then we check that the tag has been prefixed correctly
+	_, ok = m.Metadata[TagsPrefix + "t1"]
+	assert.True(t, ok)
+}
