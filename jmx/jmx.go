8e8c6d3875ef6b6bed10265713a3152af3d76339
Grigorii Merkushev <brushknight@gmail.com>
2021-01-08 12:18:46 +0100

    fix: doQuery loop exit

--- jmx/jmx.go
+++ jmx/jmx.go
@@ -323,6 +323,7 @@ func doQuery(ctx context.Context, out chan []byte, queryErrC chan error, querySt
 			queryErrC <- fmt.Errorf("reading nrjmx stdout: %s", err.Error())
 		}
 		out <- b
+		return
 	}
 }
 
