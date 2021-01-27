15bf420761cdc039bae8c8d068e9d4d30248e121
Antonio Dias <adias@newrelic.com>
2021-01-27 16:07:20 +0100

    add default nrjmx path for windows

---
+++ jmx/jmx_windows.go
@@ -0,0 +1,9 @@
+/*
+Package jmx is a library to get metrics through JMX. It requires additional
+setup. Read https://github.com/newrelic/infra-integrations-sdk#jmx-support for
+instructions. */
+package jmx
+
+const (
+	defaultNrjmxExec = "c:\\program files\\new relicº\nrjmx\\nrjmx.bat" // defaultNrjmxExec default nrjmx tool executable path
+)
