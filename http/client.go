fd582d48f9127244e5351d7d879dda6a2dc60073
Alvaro Cabanas <acabanas@newrelic.com>
2021-08-02 12:03:31 +0200

    Return early if empty flag

--- http/client.go
+++ http/client.go
@@ -14,12 +14,13 @@ import (
 	"time"
 )
 
-type clientOption func(*http.Client) error
+// ClientOption defines the format of the client option functions
+type ClientOption func(*http.Client) error
 
 // New creates a new http.Client with the Passed Client Options
 // that will have a custom certificate if it is loaded from the passed CA Bundle file and/or
 // directory. If both CABundleFile and CABundleDir are empty arguments, it creates an unsecure HTTP client.
-func New(opts ...clientOption) (*http.Client, error) {
+func New(opts ...ClientOption) (*http.Client, error) {
 	t := &http.Transport{
 		TLSClientConfig: &tls.Config{},
 	}
@@ -41,15 +42,17 @@ func New(opts ...clientOption) (*http.Client, error) {
 // if a CABundleDir is passed it will be joined to the CABundleFile to try to get the file.
 // If the file is a full path, even it the CABundleDir is passed, it will be detected and the full
 // path will be used instead
-func WithCABundleFile(CABundleFile, CABundleDir string) clientOption {
+func WithCABundleFile(CABundleFile, CABundleDir string) ClientOption {
 	return func(c *http.Client) error {
-		if CABundleFile != "" {
-			certPool := getClientCertPool(c)
-			if err := addCert(filepath.Join(CABundleDir, CABundleFile), certPool); err != nil {
-				if os.IsNotExist(err) {
-					if err = addCert(CABundleFile, certPool); err != nil {
-						return err
-					}
+		if CABundleFile == "" {
+			return nil
+		}
+
+		certPool := getClientCertPool(c)
+		if err := addCert(filepath.Join(CABundleDir, CABundleFile), certPool); err != nil {
+			if os.IsNotExist(err) {
+				if err = addCert(CABundleFile, certPool); err != nil {
+					return err
 				}
 			}
 		}
@@ -59,21 +62,23 @@ func WithCABundleFile(CABundleFile, CABundleDir string) clientOption {
 
 // WithCABundleDir adds the CABundleDir looks for pem certs in the
 // CABundleDir and adds them to the the client's certPool.
-func WithCABundleDir(CABundleDir string) clientOption {
+func WithCABundleDir(CABundleDir string) ClientOption {
 	return func(c *http.Client) error {
-		if CABundleDir != "" {
-			certPool := getClientCertPool(c)
-			files, err := ioutil.ReadDir(CABundleDir)
-			if err != nil {
-				return err
-			}
+		if CABundleDir == "" {
+			return nil
+		}
 
-			for _, f := range files {
-				if strings.Contains(f.Name(), ".pem") {
-					caCertFilePath := filepath.Join(CABundleDir, f.Name())
-					if err := addCert(caCertFilePath, certPool); err != nil {
-						return err
-					}
+		certPool := getClientCertPool(c)
+		files, err := ioutil.ReadDir(CABundleDir)
+		if err != nil {
+			return err
+		}
+
+		for _, f := range files {
+			if strings.Contains(f.Name(), ".pem") {
+				caCertFilePath := filepath.Join(CABundleDir, f.Name())
+				if err := addCert(caCertFilePath, certPool); err != nil {
+					return err
 				}
 			}
 		}
@@ -83,53 +88,55 @@ func WithCABundleDir(CABundleDir string) clientOption {
 
 // WithAcceptInvalidHostname allows the client to call the acceptInvalidHostname host
 // instead of the host from the certificates.
-func WithAcceptInvalidHostname(acceptInvalidHostname string) clientOption {
+func WithAcceptInvalidHostname(acceptInvalidHostname string) ClientOption {
 	return func(c *http.Client) error {
-		if acceptInvalidHostname != "" {
-			transport := c.Transport.(*http.Transport)
-			// Default validation is replaced with VerifyPeerCertificate.
-			// Note that when InsecureSkipVerify and VerifyPeerCertificate are in use,
-			// ConnectionState.VerifiedChains will be nil.
-			transport.TLSClientConfig.InsecureSkipVerify = true
-			// While packages like net/http will implicitly set ServerName, the
-			// VerifyPeerCertificate callback can't access that value, so it has to be set
-			// explicitly here or in VerifyPeerCertificate on the client side. If in
-			// an http.Transport DialTLS callback, this can be obtained by passing
-			// the addr argument to net.SplitHostPort.
-			transport.TLSClientConfig.ServerName = acceptInvalidHostname
-			// Approximately equivalent to what crypto/tls does normally:
-			// https://github.com/golang/go/commit/29cfb4d3c3a97b6f426d1b899234da905be699aa
-			transport.TLSClientConfig.VerifyPeerCertificate = func(certificates [][]byte, _ [][]*x509.Certificate) error {
-				certs := make([]*x509.Certificate, len(certificates))
-				for i, asn1Data := range certificates {
-					cert, err := x509.ParseCertificate(asn1Data)
-					if err != nil {
-						return errors.New("tls: failed to parse certificate from server: " + err.Error())
-					}
-					certs[i] = cert
-				}
+		if acceptInvalidHostname == "" {
+			return nil
+		}
 
-				opts := x509.VerifyOptions{
-					Roots:         transport.TLSClientConfig.RootCAs, // On the server side, use config.ClientCAs.
-					DNSName:       acceptInvalidHostname,
-					Intermediates: x509.NewCertPool(),
-					// On the server side, set KeyUsages to ExtKeyUsageClientAuth. The
-					// default value is appropriate for clients side verification.
-					// KeyUsages:     []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth},
+		transport := c.Transport.(*http.Transport)
+		// Default validation is replaced with VerifyPeerCertificate.
+		// Note that when InsecureSkipVerify and VerifyPeerCertificate are in use,
+		// ConnectionState.VerifiedChains will be nil.
+		transport.TLSClientConfig.InsecureSkipVerify = true
+		// While packages like net/http will implicitly set ServerName, the
+		// VerifyPeerCertificate callback can't access that value, so it has to be set
+		// explicitly here or in VerifyPeerCertificate on the client side. If in
+		// an http.Transport DialTLS callback, this can be obtained by passing
+		// the addr argument to net.SplitHostPort.
+		transport.TLSClientConfig.ServerName = acceptInvalidHostname
+		// Approximately equivalent to what crypto/tls does normally:
+		// https://github.com/golang/go/commit/29cfb4d3c3a97b6f426d1b899234da905be699aa
+		transport.TLSClientConfig.VerifyPeerCertificate = func(certificates [][]byte, _ [][]*x509.Certificate) error {
+			certs := make([]*x509.Certificate, len(certificates))
+			for i, asn1Data := range certificates {
+				cert, err := x509.ParseCertificate(asn1Data)
+				if err != nil {
+					return errors.New("tls: failed to parse certificate from server: " + err.Error())
 				}
-				for _, cert := range certs[1:] {
-					opts.Intermediates.AddCert(cert)
-				}
-				_, err := certs[0].Verify(opts)
-				return err
+				certs[i] = cert
 			}
+
+			opts := x509.VerifyOptions{
+				Roots:         transport.TLSClientConfig.RootCAs, // On the server side, use config.ClientCAs.
+				DNSName:       acceptInvalidHostname,
+				Intermediates: x509.NewCertPool(),
+				// On the server side, set KeyUsages to ExtKeyUsageClientAuth. The
+				// default value is appropriate for clients side verification.
+				// KeyUsages:     []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth},
+			}
+			for _, cert := range certs[1:] {
+				opts.Intermediates.AddCert(cert)
+			}
+			_, err := certs[0].Verify(opts)
+			return err
 		}
 		return nil
 	}
 }
 
 // WithInsecureSkipVerify allows the client to call any host without checking the certificates.
-func WithInsecureSkipVerify() clientOption {
+func WithInsecureSkipVerify() ClientOption {
 	return func(c *http.Client) error {
 		c.Transport.(*http.Transport).TLSClientConfig.InsecureSkipVerify = true
 		return nil
@@ -137,7 +144,7 @@ func WithInsecureSkipVerify() clientOption {
 }
 
 // WithTimeout sets the timeout for the client, if not called the timeout will be 0 (no timeout).
-func WithTimeout(httpTimeout time.Duration) clientOption {
+func WithTimeout(httpTimeout time.Duration) ClientOption {
 	return func(c *http.Client) error {
 		c.Timeout = httpTimeout
 		return nil
